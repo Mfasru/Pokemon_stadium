d9244a20342d05a803cea3ce441344b15ff55baf
Mfasru <145981725+Mfasru@users.noreply.github.com>
2023-10-09 12:32:43 +0200

    Add files via upload

---
+++ Programacion_pokemon.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package programacion_pokemon;
+
+/**
+ *
+ * @author administrador
+ */
+public class Programacion_pokemon {
+
+public class Pokemon{
+
+
+
+
+}
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
